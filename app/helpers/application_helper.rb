f352db3045ce6f01183fdb74ebde5d3dab9880bb
fernandohur <fernandohur@gmail.com>
2013-06-08 17:34:21 -0500

    se mejoraron los .css globales, se mejoror el landing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+	def render_modal_link(modal_id, class_name="tiny-image-link",&block)
+    options={:class_name=>class_name}
+    options.merge!(:modal_id=>modal_id)
+    options.merge!(:content=>capture(&block))
+    render :partial =>  "widgets/modal_link", :locals => options
+  end
+
+  def render_modal(title, modal_id, &block)
+    render :partial => "widgets/modal" , :locals => {:title => title,:modal_id=>modal_id, :content=>capture(&block)}
+  end
+
+  def render_header(title,subtitle, icon_name="")
+    render :partial => "widgets/header" , :locals => {:title=>title, :subtitle => subtitle, :icon_name=> icon_name}
+  end
+
+
 end
