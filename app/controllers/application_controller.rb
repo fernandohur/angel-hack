4d80ab97da5d5e8b698c68d649adca17747ab740
fernandohur <fernandohur@gmail.com>
2013-06-08 19:05:16 -0500

    added helpers to app controller and added script to startups/show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  def is_logged_in?
+  	return session[:user_id]
+  end
+
+  def set_logged_in_user(user_id)
+  	session[:user_id] = user_id
+  end
+
+  def get_logged_in_user
+  	return session[:user_id]
+  end
+
 end
